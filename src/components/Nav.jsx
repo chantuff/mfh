dbd0d0f6ea6a1621ac4ce9ac3a3c38dbeae88f97
Chan Tuff <chan.tuff@yahoo.com>
2023-12-05 22:02:40 -0500

    added Links

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -9,9 +9,19 @@ export default function Nav(props) {
       <Link to="/contact">
         <div>Contact Us</div>
       </Link>
+      <Link to="/give">
+        <div>Give</div>
+      </Link>
       <Link to="/about">
         <div>About</div>
       </Link>
+      <Link to="/videos">
+        <div>Videos</div>
+      </Link>
+      
+      <Link to="/pastor">
+        <div>Our Pastor</div>
+      </Link> 
     </div>
 
     )
